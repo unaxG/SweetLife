468e4b11e70afed9afefedaf3928057cb4469769
ikergorostiagala <iker.gorostiagala@elorrieta-errekamari.com>
2024-02-24 12:17:00 +0100

    0.0.1
    Icono de la aplicacion definido
    Carga de pantalla principal creada
    Login creado(falta verificacion de firebase y sharedpreferences)
    Registro creado(falta dada de alta en firebase y verificaciones como que no exista ese email y tal)

---
+++ app/src/main/java/com/example/sweetlife/Pantallas/Registro.java
@@ -0,0 +1,16 @@
+package com.example.sweetlife.Pantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sweetlife.R;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
