3f482b523312e977211204daaa5d215b7f777bbb
Andoni <andoni.fernandezlo@elorrieta-errekamari.com>
2024-02-26 12:05:59 +0100

    0.0.2 Pantalla principan diseñada(falta hacer la logica) Navegacion de login/registro a pantalla principal

---
+++ app/src/main/java/com/example/sweetlife/Pantallas/Principal.java
@@ -0,0 +1,16 @@
+package com.example.sweetlife.Pantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sweetlife.R;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
