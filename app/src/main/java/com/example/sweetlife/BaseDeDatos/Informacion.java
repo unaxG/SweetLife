2c1512ed45da6b204c8ae1cea2df6dcf456fd0f4
unaxG <unax.gonzalezga@elorrieta-errekamari.com>
2024-02-26 11:38:22 +0100

    Registro de usuario con datos firebase

---
+++ app/src/main/java/com/example/sweetlife/BaseDeDatos/Informacion.java
@@ -0,0 +1,139 @@
+package com.example.sweetlife.BaseDeDatos;
+
+public class Informacion {
+
+    private String nombre;
+    private String apellidos;
+    private String fechaNacimiento;
+    private float altura;
+    private float peso;
+    private boolean fumar;
+    private boolean problemasCorazon;
+    private boolean asma;
+    private boolean presionBaja;
+    private boolean presionAlta;
+    private boolean artritis;
+    private boolean acidoUrico;
+    private boolean diabetes;
+
+
+    public Informacion(String nombre, String apellidos, String fechaNacimiento, float peso, float altura, boolean fumar, boolean probelamasCorazon, boolean asma, boolean presionBajaa, boolean presionAlta, boolean artritis, boolean acidoUrico, boolean diabetes) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.peso = peso;
+        this.altura = altura;
+        this.fumar = fumar;
+        this.problemasCorazon = probelamasCorazon;
+        this.asma = asma;
+        this.presionBaja = presionBajaa;
+        this.presionAlta = presionAlta;
+        this.artritis = artritis;
+        this.acidoUrico = acidoUrico;
+        this.diabetes = diabetes;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public boolean isFumar() {
+        return fumar;
+    }
+
+    public void setFumar(boolean fumar) {
+        this.fumar = fumar;
+    }
+
+    public boolean isProblemasCorazon() {
+        return problemasCorazon;
+    }
+
+    public void setProblemasCorazon(boolean problemasCorazon) {
+        this.problemasCorazon = problemasCorazon;
+    }
+
+    public boolean isAsma() {
+        return asma;
+    }
+
+    public void setAsma(boolean asma) {
+        this.asma = asma;
+    }
+
+    public boolean isPresionBaja() {
+        return presionBaja;
+    }
+
+    public void setPresionBaja(boolean presionBaja) {
+        this.presionBaja = presionBaja;
+    }
+
+    public boolean isPresionAlta() {
+        return presionAlta;
+    }
+
+    public void setPresionAlta(boolean presionAlta) {
+        this.presionAlta = presionAlta;
+    }
+
+    public boolean isArtritis() {
+        return artritis;
+    }
+
+    public void setArtritis(boolean artritis) {
+        this.artritis = artritis;
+    }
+
+    public boolean isAcidoUrico() {
+        return acidoUrico;
+    }
+
+    public void setAcidoUrico(boolean acidoUrico) {
+        this.acidoUrico = acidoUrico;
+    }
+
+    public boolean isDiabetes() {
+        return diabetes;
+    }
+
+    public void setDiabetes(boolean diabetes) {
+        this.diabetes = diabetes;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+}
--- app/src/main/java/com/example/sweetlife/Pantallas/Registro.java
+++ app/src/main/java/com/example/sweetlife/Pantallas/Registro.java
@@ -1,20 +1,36 @@
 package com.example.sweetlife.Pantallas;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.sweetlife.BaseDeDatos.Informacion;
+import com.example.sweetlife.MainActivity;
 import com.example.sweetlife.R;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class Registro extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+
+    Informacion info;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 // ...
 // Initialize Firebase Auth
@@ -24,8 +40,228 @@ public class Registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
+        Button confirmar = (Button) findViewById(R.id.Registro_btnRegistrarse);
+        TextView atras = (TextView) findViewById(R.id.Registro_textViewTengoCuenta);
+
+        EditText email = (EditText) findViewById(R.id.Registro_editTextEmail);
+
+        EditText contrasenna = (EditText) findViewById(R.id.Registro_editTextContraseña);
+        EditText contrasennaRepetir = (EditText) findViewById(R.id.Registro_editTextRepetirContraseña);
+
+        //listener textview volver a login
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Registro.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //listener para el boton de crear usuario
+        confirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //sacar email y contraseña
+                String emailToString = email.getText().toString();
+                String password = contrasenna.getText().toString();
+                String passwordRep = contrasennaRepetir.getText().toString();
+
+                // si contraseñas son iguales crear usuario en AUTH
+
+                if(password.equals(passwordRep)) {
+
+                    if(crearInfo()) {
+
+
+
+
+                        mAuth.createUserWithEmailAndPassword(emailToString, password).addOnCompleteListener(Registro.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    //si funciona avisar al usuario
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+
+
+                                    builder.setTitle("Usuario creado");
+
+                                    builder.setPositiveButton("Aceptar", (DialogInterface.OnClickListener) (dialog, which) -> {
+                                        // boton de aceptar y cerrar pop-up
+
+                                        dialog.cancel();
+                                    });
+
+                                    builder.setMessage("Enhorabuena, ya es usted parte de (nombre de app)");
+                                    AlertDialog dialog = builder.create();
+                                    dialog.show();
+
+
+
+                                    enviarInfo(emailToString, info);
+
+
+
+                                } else {
+                                    // si NO funciona avisar al usuario
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+
+
+                                    builder.setTitle("Error Registrar");
+
+                                    builder.setPositiveButton("Aceptar", (DialogInterface.OnClickListener) (dialog, which) -> {
+                                        // boton de aceptar y cerrar pop-up
+
+                                        dialog.cancel();
+                                    });
+
+                                    builder.setMessage("Error al intentar crear un usuario, porfavor inserte un email y una contraseña de al menos 6 caracteres");
+                                    AlertDialog dialog = builder.create();
+                                    dialog.show();
+
+                                }
+                            }
+                        });
+
+                    }else{
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+
+
+                        builder.setTitle("Error Info");
+
+                        builder.setPositiveButton("Aceptar", (DialogInterface.OnClickListener) (dialog, which) -> {
+                            // boton de aceptar y cerrar pop-up
+
+                            dialog.cancel();
+                        });
+
+                        builder.setMessage("Error al introducir datos");
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+                }else{
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+
+
+                    builder.setTitle("La contraseña repetida es diferente");
+
+                    builder.setPositiveButton("Aceptar", (DialogInterface.OnClickListener) (dialog, which) -> {
+                        // boton de aceptar y cerrar pop-up
+
+                        dialog.cancel();
+                    });
+
+                    builder.setMessage("Parece que la contraseña que ha insertado y la repticion son diferentes, por favor insrtelos otra vez");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                }
+
+            }
+        });
+
+    };
+
+    public void enviarInfo(String usuario, Informacion e){
+
+            db.collection("Usuarios").document(usuario).set(e).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+
+
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()) {
+                        Intent i = new Intent(Registro.this, MainActivity.class);
+                        i.putExtra("usuario",usuario);
+                        startActivity(i);
+                    }
+                }
+
+
+
+            });
 
 
 
     }
+
+    public boolean crearInfo(){
+
+        info = new Informacion("","","",0, 0,false,false,false,false,false,false,false,false);
+        boolean correcto=true;
+
+        EditText nombre = (EditText) findViewById(R.id.Registro_editTextNombre);
+        String insertarDato = nombre.getText().toString();
+        info.setNombre(insertarDato);
+
+        EditText apellidos = (EditText) findViewById(R.id.Registro_editTextApellidos);
+        insertarDato = apellidos.getText().toString();
+        info.setApellidos(insertarDato);
+
+        EditText fecha = (EditText) findViewById(R.id.Registro_editTextFechaNacimiento);
+        insertarDato = fecha.getText().toString();
+        info.setFechaNacimiento(insertarDato);
+
+        EditText peso = (EditText) findViewById(R.id.Registro_editTextPeso);
+        float insertarFloat = Float.parseFloat(peso.getText().toString());
+        info.setPeso(insertarFloat);
+
+
+
+        EditText altura = (EditText) findViewById(R.id.Registro_editTextAltura);
+        insertarFloat = Float.parseFloat(altura.getText().toString());
+        info.setAltura(insertarFloat);
+
+
+
+        CheckBox fuma = (CheckBox) findViewById(R.id.Registro_checkboxFuma);
+        boolean insertarBoolean = fuma.isChecked();
+        info.setFumar(insertarBoolean);
+
+        CheckBox problemasCorazon = (CheckBox) findViewById(R.id.Registro_checkboxProblemasCorazon);
+        insertarBoolean = problemasCorazon.isChecked();
+        info.setProblemasCorazon(insertarBoolean);
+
+        CheckBox asma = (CheckBox) findViewById(R.id.Registro_checkboxAsma);
+        insertarBoolean = asma.isChecked();
+        info.setAsma(insertarBoolean);
+
+
+
+        CheckBox presionBaja = (CheckBox) findViewById(R.id.Registro_checkboxPresionArterialBaja);
+        insertarBoolean = presionBaja.isChecked();
+        info.setPresionBaja(insertarBoolean);
+
+
+
+        CheckBox presionAlta = (CheckBox) findViewById(R.id.Registro_checkboxPresionArterialAlta);
+        insertarBoolean = presionAlta.isChecked();
+        info.setPresionAlta(insertarBoolean);
+
+        CheckBox artritis = (CheckBox) findViewById(R.id.Registro_checkboxArtritis);
+        insertarBoolean = artritis.isChecked();
+        info.setArtritis(insertarBoolean);
+
+        CheckBox acidoUrico = (CheckBox) findViewById(R.id.Registro_checkboxAcidoUrico);
+        insertarBoolean = acidoUrico.isChecked();
+        info.setAcidoUrico(insertarBoolean);
+
+        CheckBox diabetes = (CheckBox) findViewById(R.id.Registro_checkboxDiabetes);
+        insertarBoolean = diabetes.isChecked();
+        info.setDiabetes(insertarBoolean);
+
+
+
+
+
+
+
+        return correcto;
+    }
+
 }
